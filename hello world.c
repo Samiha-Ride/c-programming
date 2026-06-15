8e1d0ab8347e3d2bd90d45f64e4ce227e997ce8e
Samiha Ride <samiharite2@gmail.com>
2026-06-16 04:16:39 +0600

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Samiha Rite");
+    return 0;
+}
---
+++ print your own name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter num");
+    scanf("%d", &num);
+
+    printf("%d",num);
+
+    return 0;
+}
